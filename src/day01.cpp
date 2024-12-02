8407fe90689cb2ccf29b4d81dc30ae1e9d2dd916
Alexei Zakharov <alexeiz@gmail.com>
2024-12-02 02:53:57 -0500

    solve day 1

--- src/day01.cpp
+++ src/day01.cpp
@@ -7,7 +7,69 @@ using namespace std;
 namespace {
 /// Find total distance between location id lists.
 /// https://adventofcode.com/2024/day/1
-puzzle_reg _{"1.1", []{
+puzzle_reg _1{"1.1", []{
+    using day1::loc_id_lists;
 
+    // split into individual lists
+    auto ids1 = flux::ref(loc_id_lists)
+            .map([](auto & id_pair) { return std::get<0>(id_pair); })
+            .to<std::vector>();
+
+    auto ids2 = flux::ref(loc_id_lists)
+            .map([](auto & id_pair) { return std::get<1>(id_pair); })
+            .to<std::vector>();
+
+    std::ranges::sort(ids1);
+    std::ranges::sort(ids2);
+
+    // compute distance
+    int dist = 0;
+    for (auto [id1, id2] : flux::zip(flux::ref(ids1), flux::ref(ids2)))
+        dist += std::abs(id1 - id2);
+
+    fmt::println("total distance: {}", dist);
+}};
+
+/// Calculate similarity score.
+/// https://adventofcode.com/2024/day/1#part2
+puzzle_reg _2{"1.2", []{
+    using day1::loc_id_lists;
+
+    // split into individual lists
+    auto ids1 = flux::ref(loc_id_lists)
+            .map([](auto & id_pair) { return std::get<0>(id_pair); })
+            .to<std::vector>();
+
+    auto ids2 = flux::ref(loc_id_lists)
+            .map([](auto & id_pair) { return std::get<1>(id_pair); })
+            .to<std::vector>();
+
+    std::ranges::sort(ids1);
+    std::ranges::sort(ids2);
+
+    // compute similarity
+    int sim = 0;
+    int last_val = -1;
+    int count = 0;
+    auto p1 = flux::begin(ids1);
+    auto p2 = flux::begin(ids2);
+    for (; p1 != flux::end(ids1) and p2 != flux::end(ids2); ++p1)
+    {
+        if (*p1 == last_val)
+            sim += *p1 * count;
+        else
+        {
+            for (; p2 != flux::end(ids2) && *p2 < *p1; ++p2)
+                ;
+
+            last_val = *p1;
+            for (count = 0; p2 != flux::end(ids2) && *p1 == *p2; ++p2, ++count)
+                ;
+
+            sim += *p1 * count;
+        }
+    }
+
+    fmt::println("similarity: {}", sim);
 }};
 }

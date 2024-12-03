d9c6b261c496cc9b255ef338adcabde2352abb13
Alexei Zakharov <alexeiz@gmail.com>
2024-12-02 23:17:01 -0500

    solve day 2 part 2

--- src/day02.cpp
+++ src/day02.cpp
@@ -18,7 +18,7 @@ puzzle_reg _1{"2.1", []{
     };
 
     int count_safe = 0;
-    for (auto & lev : levels)
+    for (auto & lev: levels)
     {
         auto res = flux::ref(lev).fold(
             [](safety acc, int v) -> safety {
@@ -30,6 +30,55 @@ puzzle_reg _1{"2.1", []{
                 return {acc.order == (acc.prev <=> v), acc.order, v};
             },
             safety{true, strong_ordering::equal, 0});
+        if (res.safe)
+            ++count_safe;
+    }
+
+    fmt::println("safe reports: {}", count_safe);
+}};
+
+/// Count safe level reports with dampener.
+/// https://adventofcode.com/2024/day/2#part2
+puzzle_reg _2{"2.2", []{
+    using day2::levels;
+
+    struct safety
+    {
+        bool safe;
+        strong_ordering order;
+        int prev;
+        int pos;
+    };
+
+    auto tester = [](safety acc, int v) -> safety {
+        auto [safe, order, prev, pos] = acc;
+        if (!safe) return acc;
+        if (pos == 0) return {true, order, v, pos + 1};
+        int diff = abs(v - prev);
+        if (diff < 1 or diff > 3) return {false, order, v, pos + 1};
+        if (pos == 1) return {true, prev <=> v, v, pos + 1};
+        return {order == (acc.prev <=> v), order, v, pos + 1};
+    };
+
+    int count_safe = 0;
+    for (auto & lev: levels)
+    {
+        auto res = flux::ref(lev).fold(
+            tester,
+            safety{true, strong_ordering::equal, 0, 0});
+        if (!res.safe)
+        {
+            // try without one element
+            for (auto n: flux::iota(0l, ssize(lev)))
+            {
+                res = flux::ref(lev)
+                    .filter([n=n](auto &) mutable { return n-- != 0; })
+                    .fold(tester,
+                          safety{true, strong_ordering::equal, 0, 0});
+                if (res.safe)
+                    break;
+            }
+        }
         if (res.safe)
             ++count_safe;
     }
